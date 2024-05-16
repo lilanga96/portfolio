028cb7327334367bfeb2e7b1ca7329e26c999e4e
Siphesihle Dyalvane <115469219+lilanga96@users.noreply.github.com>
2024-05-16 21:30:33 +0200

    added my About information

---
+++ src/Components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div className='info'>
+        <h2>Hello, I'm</h2>
+        <h1>Siphesihle Dyalvane</h1>
+        <h4>and I'm a Front-End Developer</h4>
+        <p>I find pleasure in translating design concepts into seamless<br></br> 
+        and interactive webpages using HTML,CSS,JavaScript and React</p>
+        </div>
+        <div className='picture'>
+            <img src='./src/images/pro-picture.jpg'></img>
+        </div>
+    </div>
+  )
+}
+
+export default Home
