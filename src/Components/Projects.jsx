e6f37212a7587aa0680e08049ce772d7becb4cfd
Siphesihle Dyalvane <115469219+lilanga96@users.noreply.github.com>
2024-05-22 07:57:30 +0200

    fixing an issue with imports

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,7 @@
 
 import './Project.css';
 import React from 'react';
-import projectsData from './projectsData.jsx'
+import projectsData from './projectsData.jsx';
 
 const Projects = () => {
     return (
