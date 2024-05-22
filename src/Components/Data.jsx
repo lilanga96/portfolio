9800ce9e220c31d0d9dfb828720e841474ea994a
Siphesihle Dyalvane <115469219+lilanga96@users.noreply.github.com>
2024-05-22 09:37:44 +0200

    image path update

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -2,13 +2,13 @@
 const projectsData = [
   {
       title: 'Podcast-app',
-      image:  '/src/images/books2.png',
+      image:  '//images/books2.png',
       liveLink: 'https://podcastshubb.netlify.app',
       githubLink: 'https://github.com/lilanga96/podcasts'
   },
   {
       title: 'Book-connect',
-      image: '/src/images/books.png', 
+      image: '/images/books.png', 
       liveLink: 'https://book-konnect.netlify.app',
       githubLink: 'https://github.com/lilanga96/Book-connect'
   },
